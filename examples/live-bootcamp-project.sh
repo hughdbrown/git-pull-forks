af270f7ab6aced1760db7496025817b639b9050e
Hugh Brown <hughdbrown@yahoo.com>
2025-09-03 12:44:40 -0600

    Added examples

---
+++ examples/live-bootcamp-project.sh
@@ -0,0 +1,137 @@
+['live-bootcamp-project']
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/aldass' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/aldass' && git clone https://github.com/aldass/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/paragoner1' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/paragoner1' && git clone https://github.com/paragoner1/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/Ripper53' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/Ripper53' && git clone https://github.com/Ripper53/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/rgruberski' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/rgruberski' && git clone https://github.com/rgruberski/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/raywu-hk' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/raywu-hk' && git clone https://github.com/raywu-hk/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/mpdevilleres' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/mpdevilleres' && git clone https://github.com/mpdevilleres/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/MoonKraken' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/MoonKraken' && git clone https://github.com/MoonKraken/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/mmcclatchy' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/mmcclatchy' && git clone https://github.com/mmcclatchy/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/mjohnson518' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/mjohnson518' && git clone https://github.com/mjohnson518/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/MikhailSporyshev' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/MikhailSporyshev' && git clone https://github.com/MikhailSporyshev/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/mattjsharp' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/mattjsharp' && git clone https://github.com/mattjsharp/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/MadMathMike' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/MadMathMike' && git clone https://github.com/MadMathMike/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/luisgabrielmendez' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/luisgabrielmendez' && git clone https://github.com/luisgabrielmendez/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/luiscarlosjayk' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/luiscarlosjayk' && git clone https://github.com/luiscarlosjayk/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/lfbos' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/lfbos' && git clone https://github.com/lfbos/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/KalyniukV' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/KalyniukV' && git clone https://github.com/KalyniukV/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/WallyOxen' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/WallyOxen' && git clone https://github.com/WallyOxen/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/VimCommando' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/VimCommando' && git clone https://github.com/VimCommando/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/vikramriyer' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/vikramriyer' && git clone https://github.com/vikramriyer/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/verygreenboi' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/verygreenboi' && git clone https://github.com/verygreenboi/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/TuinderJ' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/TuinderJ' && git clone https://github.com/TuinderJ/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/tiberiumboy' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/tiberiumboy' && git clone https://github.com/tiberiumboy/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/thedrummeraki' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/thedrummeraki' && git clone https://github.com/thedrummeraki/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/The-Rabak' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/The-Rabak' && git clone https://github.com/The-Rabak/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/tedkimdev' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/tedkimdev' && git clone https://github.com/tedkimdev/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/tahoe' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/tahoe' && git clone https://github.com/tahoe/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/Sweng0d' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/Sweng0d' && git clone https://github.com/Sweng0d/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/srih1rsh1' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/srih1rsh1' && git clone https://github.com/srih1rsh1/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/srevesnerol' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/srevesnerol' && git clone https://github.com/srevesnerol/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/smk1992' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/smk1992' && git clone https://github.com/smk1992/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/shafiquejamal' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/shafiquejamal' && git clone https://github.com/shafiquejamal/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/serbanrobu' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/serbanrobu' && git clone https://github.com/serbanrobu/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/scopevale' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/scopevale' && git clone https://github.com/scopevale/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/DUNNSEAN' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/DUNNSEAN' && git clone https://github.com/DUNNSEAN/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/dpdresser' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/dpdresser' && git clone https://github.com/dpdresser/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/dobleuber' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/dobleuber' && git clone https://github.com/dobleuber/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/deanmiller' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/deanmiller' && git clone https://github.com/deanmiller/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/CyndieKamau' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/CyndieKamau' && git clone https://github.com/CyndieKamau/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/crttcr' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/crttcr' && git clone https://github.com/crttcr/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/coolboole' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/coolboole' && git clone https://github.com/coolboole/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/cj-zhukov' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/cj-zhukov' && git clone https://github.com/cj-zhukov/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/chadangelelli' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/chadangelelli' && git clone https://github.com/chadangelelli/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/bloodfeast' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/bloodfeast' && git clone https://github.com/bloodfeast/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/BeyondJacob' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/BeyondJacob' && git clone https://github.com/BeyondJacob/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/andreinnanu' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/andreinnanu' && git clone https://github.com/andreinnanu/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/alexlave100' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/alexlave100' && git clone https://github.com/alexlave100/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/aleanon' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/aleanon' && git clone https://github.com/aleanon/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/adamNewell' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/adamNewell' && git clone https://github.com/adamNewell/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/acgetchell' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/acgetchell' && git clone https://github.com/acgetchell/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/0xdecr1pto' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/0xdecr1pto' && git clone https://github.com/0xdecr1pto/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/JWLee89' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/JWLee89' && git clone https://github.com/JWLee89/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/jocax' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/jocax' && git clone https://github.com/jocax/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/JiriMON' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/JiriMON' && git clone https://github.com/JiriMON/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/jasonpark94' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/jasonpark94' && git clone https://github.com/jasonpark94/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/jakubsuchybio' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/jakubsuchybio' && git clone https://github.com/jakubsuchybio/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/jakegny' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/jakegny' && git clone https://github.com/jakegny/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/ipcasj' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/ipcasj' && git clone https://github.com/ipcasj/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/Idleness76' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/Idleness76' && git clone https://github.com/Idleness76/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/hnariman' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/hnariman' && git clone https://github.com/hnariman/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/helloiamgp' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/helloiamgp' && git clone https://github.com/helloiamgp/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/harryobas' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/harryobas' && git clone https://github.com/harryobas/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/hank-butler' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/hank-butler' && git clone https://github.com/hank-butler/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/GuillemIscla' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/GuillemIscla' && git clone https://github.com/GuillemIscla/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/garikAsplund' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/garikAsplund' && git clone https://github.com/garikAsplund/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/gabrielhsc95' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/gabrielhsc95' && git clone https://github.com/gabrielhsc95/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/gaborzatik' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/gaborzatik' && git clone https://github.com/gaborzatik/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/evanmiller2112' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/evanmiller2112' && git clone https://github.com/evanmiller2112/live-bootcamp-project.git)
+# ------------------------------
+(mkdir -p '/Users/hughbrown/workspace/forks/live-bootcamp-project/evalredacted' && cd '/Users/hughbrown/workspace/forks/live-bootcamp-project/evalredacted' && git clone https://github.com/evalredacted/live-bootcamp-project.git)
